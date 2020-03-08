53a36483b52970a049a2c726938341b0caace4e5
giraudier pierre <giraudier.pierre@gmail.com>
2020-03-08 14:13:18 +0100

    setup localStorage for itemsList

---
+++ my-app/src/components/atoms/AddButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddButton() {
+    return(
+        <a href="/add">
+            <span className="add-button">+</span>
+        </a>
+    );
+}
+
+export default AddButton;
--- my-app/src/components/organisms/List.js
+++ my-app/src/components/organisms/List.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ListItem from './ListItem';
+import HomeHeaderBar from '../molecules/HomeHeaderBar';
+
+/* Mock data */
+function setLocalStorageData() {
+    var data = [
+        {
+            key: 0,
+            title: 'Titre1',
+        },
+        {
+            key: 1,
+            title: 'Titre2',
+        },
+        {
+            key: 2,
+            title: 'Titre3',
+        },
+        {
+            key: 3,
+            title: 'Titre4',
+        },
+        {
+            key: 4,
+            title: 'Titre5',
+        },
+        {
+            key: 5,
+            title: 'Titre6',
+        },
+        {
+            key: 6,
+            title: 'Titre7',
+        },
+        {
+            key: 7,
+            title: 'Titre8',
+        }
+    ];
+
+    localStorage.setItem('listItems', JSON.stringify(data));
+}
+
+function getLocalStorageData() {
+    var items = JSON.parse(localStorage.getItem('listItems'));
+    return items;
+}
 
 function List() {
     return(
-        <div className="list">
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
+        <div>
+            <HomeHeaderBar />
+            <div className="list">
+                {getLocalStorageData().map(function(item, key) {
+                    return(
+                        <ListItem 
+                            key={key}
+                            title={item.title}
+                        />
+                    )
+                })}
+            </div>
         </div>
     )
 }
+
+setLocalStorageData();
 
 export default List;
